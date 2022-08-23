ee6a2166b7ee2d1c88deb3017790f4ddd72bbb2d
dhavanikgithub <kanzariyagovindbhai21@gnu.ac.in>
2022-08-23 17:38:28 +0530

    MADPractical1_21012022022

--- src/main/kotlin/practical1.7.kt
+++ src/main/kotlin/practical1.7.kt
@@ -2,7 +2,10 @@ import java.math.BigInteger
 
 fun main()
 {
-    print("Factorial is: ${fact(BigInteger("10"))}")
+    print("Enter Number: ")
+    var num:BigInteger = readLine()!!.toBigInteger()
+    println("Factorial is: ${fact2(num)}")
+    println("With using tailrec Factorial is: ${fact(num)}")
 }
 tailrec fun fact(n: BigInteger, temp: BigInteger = BigInteger("1")): BigInteger {
     return if (n == BigInteger("1")){
@@ -11,3 +14,11 @@ tailrec fun fact(n: BigInteger, temp: BigInteger = BigInteger("1")): BigInteger 
         fact(n-BigInteger("1"), temp*n)
     }
 }
+fun fact2(n: BigInteger, temp: BigInteger = BigInteger("1")): BigInteger {
+    return if (n == BigInteger("1")){
+        temp
+    } else {
+        fact(n-BigInteger("1"), temp*n)
+    }
+}
+
